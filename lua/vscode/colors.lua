66d143e78e26b2c6bf04eef856a07c4e1fcc346a
Marco van den Oever <marco@flyingpie.nl>
2026-02-28 11:38:47 +0100

    Workspace

--- lua/vscode/colors.lua
+++ lua/vscode/colors.lua
@@ -30,6 +30,7 @@ colors.get_colors = function()
             vscCursorLight = '#AEAFAD',
             vscSelection = '#264F78',
             vscLineNumber = '#5A5A5A',
+            vscNonText = '#222222',
             vscWhitespace = '#222222',
 
             vscDiffRedDark = '#4B1818',
